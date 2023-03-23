c10da76574c41fbb0268336740fb40f3bb541088
Abidin Darıcı <114923176+abidindarici@users.noreply.github.com>
2023-03-24 02:35:22 +0300

    Add files via upload

---
+++ C#-Switch Case/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+          private static void Main(string[] args)
+          {
+                    int month = DateTime.Now.Month;
+                    
+                    //expression
+                    switch (month)
+                    {
+                              case 1:
+                                        Console.WriteLine("Ocak Ayındasınız!");
+                                        break;
+                              case 2:
+                                        Console.WriteLine("Şubat Ayındasınız!");
+                                        break;
+                              case 3:
+                                        Console.WriteLine("Mart Ayındasınız!");
+                                        break;
+                              case 4:
+                                        Console.WriteLine("Nisan Ayındasınız!");
+                                        break;
+                              default:
+                                        Console.WriteLine("Yanlış Veri Girişi!");
+                              break;
+                    }
+                    
+                    switch (month)
+                    {
+                              case 12:
+                              case 1:
+                              case 2:
+                                        System.Console.WriteLine("Kış Ayındasınız...");
+                                        break;
+                              case 3:
+                              case 4:
+                              case 5:
+                                        System.Console.WriteLine("İlkbahar Ayındasınız...");
+                                        break;
+                              case 6:
+                              case 7:
+                              case 8:
+                                        System.Console.WriteLine("Yaz Ayındasınız...");
+                                        break;
+                              case 9:
+                              case 10:
+                              case 11:
+                                        System.Console.WriteLine("Sonbahar Ayındasınız...");
+                                        break;
+                              default:
+                              break;
+                    }
+
+          }
+}
